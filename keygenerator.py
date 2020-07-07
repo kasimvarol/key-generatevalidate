591b2fa5cd94528d5d918823d7b06dcaa299783f
kasimvarol <kasim.varol@ug.bilkent.edu.tr>
2020-07-07 08:44:03 +0300

    functionalized

--- keygenerator.py
+++ keygenerator.py
@@ -1,44 +1,52 @@
 import subprocess
 
-arrbit = [2048, 4096, 8192]
-choice = 0
+def createKey(bit, days, country, state, city, ou, cn):
+    cmd1 = 'openssl req \
+        -new \
+        -newkey rsa:{bit} \
+        -days {days} \
+        -nodes \
+        -x509 \
+        -subj "/C={country}/ST={state}/L={city}/O={ou}/CN={cn}" \
+        -keyout {cn}.key \
+        -out {cn}.crt'.format(bit=bit, days=days, country=country, state=state, city=city, ou=ou, cn=cn)
+    output = subprocess.check_output(['bash','-c', cmd1])
 
-while choice<1 or choice>3:
-    choice = int(input('Pick a bit level 1=2048, 2=4096, 3=8192 (1,2,3):'))
-bit=arrbit[choice]
+def createCrt(country, state, city, ou, cn):
+    cmd2 = 'openssl req \
+        -new \
+        -subj "/C={country}/ST={state}/L={city}/O={ou}/CN={cn}" \
+        -key {cn}.key \
+        -out {cn}.csr'.format(country=country, state=state, city=city, ou=ou, cn=cn)
+    output = subprocess.check_output(['bash','-c', cmd2])
 
-days = int(input('How many DAYS valid for:'))
-print("Enter above subjects.")
-country = input('Country:')
-state = input('State:')
-city = input('City:')
-ou = input('Organizational Unit:')
-cn = input('CN:')
+def CreateClientCrt(days, cn):
+    cmd3 = 'openssl x509 -req -days {days} -in {cn}.csr -CA {cn}.crt -CAkey {cn}.key -CAcreateserial -out {cn}-client.crt'.format(days=days, cn=cn)
+    output3 = subprocess.call(cmd3, shell=True)
 
-cmd1 = 'openssl req \
-    -new \
-    -newkey rsa:{bit} \
-    -days {days} \
-    -nodes \
-    -x509 \
-    -subj "/C={country}/ST={state}/L={city}/O={ou}/CN={cn}" \
-    -keyout {cn}.key \
-    -out {cn}.crt'.format(bit=bit, days=days, country=country, state=state, city=city, ou=ou, cn=cn)
-output = subprocess.check_output(['bash','-c', cmd1])
+def VerifyCrt:
+    cmd4 = 'openssl verify -CAfile {cn}.crt {cn}-client.crt'.format(cn=cn)
+    # output4 = subprocess.check_output(['bash','-c', cmd4])
+    result = subprocess.Popen(cmd4, shell = True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    dummy,res=result.communicate()
+    print (res)
 
-cmd2 = 'openssl req \
-    -new \
-    -subj "/C={country}/ST={state}/L={city}/O={ou}/CN={cn}" \
-    -key {cn}.key \
-    -out {cn}.csr'.format(country=country, state=state, city=city, ou=ou, cn=cn)
-output = subprocess.check_output(['bash','-c', cmd2])
+def PfxtoCrt(cn):
+    cmd5 = 'openssl pkcs12 -export -out {cn}.pfx -inkey {cn}.key -in {cn}.crt'.format(cn=cn)
+    li = list(cmd5.split(" ")) 
+    subprocess.run(li)
 
-cmd3 = 'openssl x509 -req -days {days} -in {cn}.csr -CA {cn}.crt -CAkey {cn}.key -CAcreateserial -out {cn}-client.crt'.format(days=days, cn=cn)
-output3 = subprocess.call(cmd3, shell=True)
+def checkcrt(cert):
+    with open(cert, "r") as f:
+        first = f.readline()
+        for last in f: pass
 
-cmd4 = 'openssl verify -CAfile {cn}.crt {cn}-client.crt'.format(cn=cn)
-# output4 = subprocess.check_output(['bash','-c', cmd4])
-result = subprocess.Popen(cmd4, shell = True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    beg = "-----BEGIN CERTIFICATE-----\n"
+    end = "-----END CERTIFICATE-----\n"
 
-output,res=result.communicate()
-print (res)
+
+    if beg==first and last==end:
+        print("Certification struct looks okay.")
+    else:
+        print("It is invalid.")
+
